01b0d21d98fc66f344c331efc3ce40fe5f4db75d
jackchensm <1655664358@qq.com>
2019-05-10 17:43:56 +0800

    feat(video):resolve video

--- app/Listeners/addVideoListener.php
+++ app/Listeners/addVideoListener.php
@@ -27,7 +27,7 @@ class addVideoListener
     public function handle(addVideo $event)
     {
         $video = $event->videoModel;
-        $localFileSrc = config("upload")['attachedDir']."/".str_replace('/','',$video);
+        $localFileSrc = config("upload")['attachedDir']."/".str_replace('/','',$video['uriKey']);
         $cosFile = downloadCosFileSavelocal($video['uriKey'],$localFileSrc);
         $ffmpeg = \FFMpeg\FFMpeg::create(array(
             'ffmpeg.binaries'  => '/usr/local/bin/ffmpeg',

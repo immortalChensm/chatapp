fce8a7a2bd9da25c875fe755d51bbaa2c6f28bad
jackchensm <1655664358@qq.com>
2019-05-23 17:38:57 +0800

    fix(table):update praise table of status filed

--- database/migrations/2019_05_23_074009_create_praise_table.php
+++ database/migrations/2019_05_23_074009_create_praise_table.php
@@ -20,7 +20,7 @@ class CreatePraiseTable extends Migration
             $table->tinyInteger("modelType")->comment("1为文章2图片3音乐4视频");
             $table->unsignedBigInteger("praiseUserId")->index()->comment("被点赞的用户id");
             $table->tinyInteger("praiseType")->comment("赞类型1为支持2为踩");
-            $table->tinyInteger("status")->comment("1为踩或赞状态0为未赞或啃状态即取消");
+            $table->tinyInteger("status")->default(0)->nullable()->comment("1为踩或赞状态0为未赞或啃状态即取消");
             $table->dateTime("day")->index()->comment("点赞日期");
             $table->timestamps();
         });

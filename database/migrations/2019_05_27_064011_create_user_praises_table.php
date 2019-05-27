fbe212704894d1639931a9dad5a2f014f14e991e
jackchensm <1655664358@qq.com>
2019-05-27 14:55:26 +0800

    feat(table):append user praise table

---
+++ database/migrations/2019_05_27_064011_create_user_praises_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPraisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_praises', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger("userId")->index()->comment("用户id");
+            $table->unsignedBigInteger("praiseNum")->default(0)->comemt("用户获得的点赞数量");
+            $table->date("day")->comment("日期");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_praises');
+    }
+}

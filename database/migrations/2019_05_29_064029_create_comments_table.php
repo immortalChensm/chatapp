7e8f30afe75c76cb4b45f6ae991741caa046b435
jackchensm <1655664358@qq.com>
2019-05-29 14:48:29 +0800

    feat(table):添加评论表

---
+++ database/migrations/2019_05_29_064029_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger("userId")->index()->comment("评论者id");
+            $table->unsignedBigInteger("modelId")->index()->comment("评论的数据id");
+            $table->unsignedTinyInteger("modelType")->index()->comment("1文章2相册3音乐4视频");
+            $table->unsignedBigInteger("ownerUserId")->index()->comment("被评论的用户id");
+            $table->text("content")->comment("评论内容");
+            $table->unsignedInteger("praise")->comment("评论的支持数");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}

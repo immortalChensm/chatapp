d3380bfffc2430f69b86c9af0b51aa6948ab4dd2
jackchensm <1655664358@qq.com>
2019-06-01 16:30:30 +0800

    feat(comment):comment

--- app/Http/Controllers/Admin/CommentsController.php
+++ app/Http/Controllers/Admin/CommentsController.php
@@ -41,8 +41,9 @@ class CommentsController extends Controller
             $item->commentUserName = User::where("userId", $item['ownerUserId'])->value("name");
             $item->typeName        = $this->typeTitle[$item->modelType];
             $item->commentReply    = count($item->reply);
-            if ($item['isDeleted']==1)unset($item);
             $item->isShow = $item->isShow==1?'否':'是';
+            if ($item['isDeleted']==1)unset($item);
+
         }]);
     }
 

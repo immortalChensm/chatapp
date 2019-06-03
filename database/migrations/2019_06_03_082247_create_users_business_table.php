c9f63a7611c55f1a336a44005c620b68f557afb7
jackchensm <1655664358@qq.com>
2019-06-03 16:58:44 +0800

    feat(table):业务表更新

--- database/migrations/2019_06_03_082247_create_users_business_table.php
+++ database/migrations/2019_06_03_082247_create_users_business_table.php
@@ -15,6 +15,7 @@ class CreateUsersBusinessTable extends Migration
     {
         Schema::create('users_business', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedBigInteger("userId")->index()->comment("用户id");
             $table->string("name",30)->index()->comment("姓名");
             $table->string("mobile",11)->index()->comment("电话");
             $table->string("address",200)->comment("地址");

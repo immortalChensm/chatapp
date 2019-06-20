d796866db2d9525ae362a600e8295f962b6149e4
jackchensm <1655664358@qq.com>
2019-06-20 14:08:47 +0800

    feat(redpacket):红包领取表

---
+++ database/migrations/2019_06_20_060551_create_users_redpacket_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersRedpacketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_redpacket', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger("userId")->index()->comment("红包领取人");
+            $table->unsignedBigInteger("sendUserId")->index()->comment("红包发送人");
+            $table->unsignedDecimal("money",10,2)->default(0)->comment("红包领取金额");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_redpacket');
+    }
+}

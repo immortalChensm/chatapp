80fa091ba5da17c428bd0e67aff3b87931daeb9b
jackchensm <1655664358@qq.com>
2019-06-25 16:51:07 +0800

    feat(table):update group table

--- database/migrations/2019_06_24_080002_create_groups_table.php
+++ database/migrations/2019_06_24_080002_create_groups_table.php
@@ -20,6 +20,11 @@ class CreateGroupsTable extends Migration
             $table->string("Operator_Account")->comment("创建者");
             $table->unsignedBigInteger("Owner_Account")->index()->comment("群主id");
             $table->string("Type")->index()->comment("群类型");
+            $table->string("Introduction")->comment("群简介");
+            $table->string("FaceUrl")->comment("群头像");
+            $table->unsignedInteger("MemberNum")->default(0)->comment("目前人数");
+            $table->unsignedInteger("LastMsgTime")->default(0)->comment("当前群的最后活跃时间");
+            $table->unsignedInteger("CreateTime")->default(0)->comment("当前群的创建时间");
             $table->timestamps();
         });
     }

26824fd035d04a8313348e3fb61b93fef63abe70
jackchensm <1655664358@qq.com>
2019-06-29 16:21:57 +0800

    group

--- app/Http/Controllers/Admin/GroupsController.php
+++ app/Http/Controllers/Admin/GroupsController.php
@@ -39,7 +39,7 @@ class GroupsController extends Controller
 
     function groupSendMsg(Request $request)
     {
-        if (empty($request['message']))return ['code'=>0,'message'=>'请填写消息内容'];
+        if (empty($request['Text']))return ['code'=>0,'message'=>'请填写消息内容'];
         $result = $this->getApi("POST","api/im/group/sendMsg",request()->except(['s','_token']));
         return ['code'=>1,'message'=>$result];
     }

e2a73ba0ebb27473ebc45768150ef3750d5fff2b
jackchensm <1655664358@qq.com>
2019-07-01 17:11:52 +0800

    users edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -37,7 +37,7 @@
                                     <label for="inputEmail3" class="col-sm-2 control-label">用户头像</label>
 
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control"  value="@if(isset($data['headImgUrl'])) {{$data['headImgUrl']}} @endif">
+                                        <input type="text" class="form-control"  style="border: none" value="@if(isset($data['headImgUrl'])) {{$data['headImgUrl']}} @endif">
                                     </div>
                                 </div>
 

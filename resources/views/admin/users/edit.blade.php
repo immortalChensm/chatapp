8f3cc178d028c44feb027df42ebc680f12fd2388
jackchensm <1655664358@qq.com>
2019-07-01 16:41:52 +0800

    group

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -33,48 +33,23 @@
                         <form role="form" id="postForm">
                             {{csrf_field()}}
 
-                            <input type="hidden" name="articleId" value=" @if(isset($data['articleId'])){{$data['articleId']}} @endif">
+                            <input type="hidden" name="articleId" value=" @if(isset($data['userId'])){{$data['userId']}} @endif">
 
                             <div class="box-body">
 
-
                                 <div class="form-group edit-box">
-                                    <label>文章标签</label>
-                                    <select class="form-control" name="tagId"  required="required">
-                                        <option value="">请选择文章标签</option>
-                                        @if(!empty($tag))
-                                            @foreach($tag as $item)
-                                        <option value="{{$item['tagId']}}" @if(isset($data['articleId'])&&$item['tagId']==$data['tagId']) selected @endif>{{$item['name']}}</option>
-                                            @endforeach
-                                            @endif
-                                    </select>
+                                    <label for="exampleInputEmail1">传联号</label>
+                                    <input type="text" class="form-control input-max-box" class="edit-box" name="handNum" value="@if(isset($data['handNum'])) {{$data['handNum']}} @endif">
                                 </div>
 
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">文章标题</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="title" value="@if(isset($data['title'])) {{$data['title']}} @endif" placeholder="文章标题">
                                 </div>
+                                <!-- /.box-body -->
 
-
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">文章内容</label>
-
-                                    <textarea id="editor" name="content" value="@if(isset($data['articleId'])){{$data['content']}}@endif" style="width:1000px;height:450px;">
-                                      @if(isset($data['articleId'])) {{$data['content']}} @endif
-                                    </textarea>
-
-
-
-</div>
-
-</div>
-<!-- /.box-body -->
-
-<div class="box-footer">
-<a  class="btn btn-success" onclick="store()">提交</a>
-<a href="{{url()->previous()}}" class="btn btn-info">返回</a>
-</div>
-</form>
+                                <div class="box-footer">
+                                <a  class="btn btn-success" onclick="store()">提交</a>
+                                <a href="{{url()->previous()}}" class="btn btn-info">返回</a>
+                                </div>
+                                </form>
 </div>
 
 

6f0f6449387ea097ca1126186ffbfed7669370e4
jackchensm <1655664358@qq.com>
2019-07-01 17:05:36 +0800

    users edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -30,171 +30,39 @@
                         <div class="box-header with-border">
                             {{--<h3 class="box-title">Quick Example</h3>--}}
                         </div>
-                        <form role="form" id="postForm">
-                            {{csrf_field()}}
-
-                            <input type="hidden" name="userId" value=" @if(isset($data['userId'])){{$data['userId']}} @endif">
-
+                        <form class="form-horizontal">
                             <div class="box-body">
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户头像</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="headImgUrl" value="@if(isset($data['headImgUrl'])) {{$data['headImgUrl']}} @endif">
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                    </div>
                                 </div>
+                                <div class="form-group">
+                                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
 
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">传联号</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="handNum" value="@if(isset($data['handNum'])) {{$data['handNum']}} @endif">
+                                    <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                    </div>
                                 </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户昵称</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="name" value="@if(isset($data['name'])) {{$data['name']}} @endif">
+                                <div class="form-group">
+                                    <div class="col-sm-offset-2 col-sm-10">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> Remember me
+                                            </label>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户星级</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="star" value="@if(isset($data['star'])) {{$data['star']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">真实姓名</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="realName" value="@if(isset($data['realName'])) {{$data['realName']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">性别</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="sex" value="@if(isset($data['sex'])) {{$data['sex']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">手机号码</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="mobile" value="@if(isset($data['mobile'])) {{$data['mobile']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">是否认证</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="isValiated" value="@if(isset($data['isValiated'])) {{$data['isValiated']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">账户余额</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="money" value="@if(isset($data['money'])) {{$data['money']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">船票余额</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="shipNumber" value="@if(isset($data['shipNumber'])) {{$data['shipNumber']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">累积点赞数量</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="praiseNumber" value="@if(isset($data['praiseNumber'])) {{$data['praiseNumber']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">推广码</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="spreadCode" value="@if(isset($data['spreadCode'])) {{$data['spreadCode']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户生日</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="birthday" value="@if(isset($data['birthday'])) {{$data['birthday']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户小名</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="petName" value="@if(isset($data['petName'])) {{$data['petName']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户出生地</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="birthPlaceProvinceId" value="@if(isset($data['birthPlaceProvinceId'])) {{$data['birthPlaceProvinceId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户现居住地</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="locationProvinceId" value="@if(isset($data['locationProvinceId'])) {{$data['locationProvinceId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户介绍</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="introduction" value="@if(isset($data['introduction'])) {{$data['introduction']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">身份证号码</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="idCard" value="@if(isset($data['idCard'])) {{$data['idCard']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">身份证照片</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="idCardFrontPic" value="@if(isset($data['idCardFrontPic'])) {{$data['idCardFrontPic']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">是否是IM用户</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="isIm" value="@if(isset($data['isIm'])) {{$data['isIm']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">推荐人</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户红包支出总金额</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">用户红包收入总金额</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止账号登录</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止发布文章</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止发布相册</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止发布音乐</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止发布视频</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">禁止评论内容</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="recommendUserId" value="@if(isset($data['recommendUserId'])) {{$data['recommendUserId']}} @endif">
-                                </div>
-
-                                <div class="form-group edit-box">
-                                    <label for="exampleInputEmail1">注册时间</label>
-                                    <input type="text" class="form-control input-max-box" class="edit-box" name="created_at" value="@if(isset($data['created_at'])) {{$data['created_at']}} @endif">
-                                </div>
-
-                                </div>
-                                <!-- /.box-body -->
-
-                                <div class="box-footer">
-                                <a  class="btn btn-success" onclick="store()">提交</a>
-                                <a href="{{url()->previous()}}" class="btn btn-info">返回</a>
-                                </div>
-                                </form>
+                            </div>
+                            <!-- /.box-body -->
+                            <div class="box-footer">
+                                <button type="submit" class="btn btn-default">Cancel</button>
+                                <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                            </div>
+                            <!-- /.box-footer -->
+                        </form>
 </div>
 
 

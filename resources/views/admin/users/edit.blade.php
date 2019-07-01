22b66583da08458b152a15137328272e6b644c09
jackchensm <1655664358@qq.com>
2019-07-01 17:08:34 +0800

    users edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -32,29 +32,24 @@
                         </div>
                         <form class="form-horizontal">
                             <div class="box-body">
+
                                 <div class="form-group">
-                                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                                    <label for="inputEmail3" class="col-sm-2 control-label">用户头像</label>
 
                                     <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                        <input type="email" class="form-control"  value="@if(isset($data['headImgUrl'])) {{$data['headImgUrl']}} @endif id="inputEmail3" placeholder="Email">
                                     </div>
                                 </div>
+
                                 <div class="form-group">
-                                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                                    <label for="inputPassword3" class="col-sm-2 control-label">传联号</label>
 
                                     <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                        <input type="password" class="form-control" id="inputPassword3" value="@if(isset($data['handNum'])) {{$data['handNum']}} @endif placeholder="Password">
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"> Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
+
+
                             </div>
                             <!-- /.box-body -->
                             <div class="box-footer">

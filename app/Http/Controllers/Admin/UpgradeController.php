1f8a290889d4938537fb3a52d26af6b0e2c0aaaf
jackchensm <1655664358@qq.com>
2019-07-02 14:25:21 +0800

    upgrade

---
+++ app/Http/Controllers/Admin/UpgradeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Upgrade;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UpgradeController extends Controller
+{
+
+    function index()
+    {
+        return view("admin.upgrade.index");
+    }
+    function upgrade(Request $request,Upgrade $upgrade)
+    {
+        return $this->models(...[$request,$upgrade,function (&$searchItem)use($request){
+            if (!empty($request->query->get('name'))){
+                $userIds = User::where("name","LIKE","%".$request->query->get('name')."%")->pluck("userId");
+                $searchItem['userId']   = count($userIds->toArray())>0?$userIds->toArray():'';
+            }
+        },function ($query,&$searchItem){
+            if (isset($searchItem['userId'])&&!empty($searchItem['userId'])){
+                $query->whereIn("userId",$searchItem['userId']);
+            }
+        },function (&$item){
+            $item->userName        = User::where("userId", $item['userId'])->value("name");
+            $item->createdDate     = date("Y-m-d H", strtotime($item->created_at));
+        }]);
+    }
+
+    function remove(Upgrade $upgrade)
+    {
+        return $this->removeModel($upgrade,1);
+    }
+}
---
+++ app/Upgrade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Upgrade extends Model
+{
+    //
+    protected $table = "upgrade";
+    public $primaryKey = "id";
+    protected $guarded = [];
+}

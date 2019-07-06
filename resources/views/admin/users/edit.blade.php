96a88f144ea1fc166a3591fc91016eca8152e088
jackchensm <1655664358@qq.com>
2019-07-06 16:45:56 +0800

    admin

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -345,10 +345,7 @@
                                     <label class="">
                                         <div class="iradio_flat-green checked" aria-checked="true" aria-disabled="false" style="position: relative;"><input type="radio" name="r3" class="flat-red" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
                                     </label>
-                                    <label>
-                                        <div class="iradio_flat-green disabled" aria-checked="false" aria-disabled="true" style="position: relative;"><input type="radio" name="r3" class="flat-red" disabled="" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
-                                        Flat green skin radio
-                                    </label>
+                                  
                                 </div>
 
                             </div>
@@ -399,7 +396,7 @@
             radioClass   : 'iradio_flat-green'
         })
 
-        
+
     </script>
 
 @endsection

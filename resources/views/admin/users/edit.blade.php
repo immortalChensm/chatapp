6309ded285601e72ca92e199f1fee2ca507cb0cb
jackchensm <1655664358@qq.com>
2019-07-23 15:34:08 +0800

    user

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -402,7 +402,7 @@
             radioClass   : 'iradio_flat-green'
         })
 
-        var permissionBtnSetting = {
+        $("[name='login-checkbox'],[name='post-checkbox'],[name='photo-checkbox'],[name='music-checkbox'],[name='video-checkbox'],[name='comment-checkbox']").bootstrapSwitch({
             onText : "启用",      // 设置ON文本  
             offText : "禁用",    // 设置OFF文本  
             onColor : "success",// 设置ON文本颜色     (info/success/warning/danger/primary)  
@@ -410,9 +410,6 @@
             size : "mini",    // 设置控件大小,从小到大  (mini/small/normal/large)  
             handleWidth:"35",//设置控件宽度
             state:false,
-        };
-        $("[name='login-checkbox']").bootstrapSwitch({
-            permissionBtnSetting,
             // 当开关状态改变时触发  
             onSwitchChange : function(event, state) {
                 var data = event.target.defaultValue;

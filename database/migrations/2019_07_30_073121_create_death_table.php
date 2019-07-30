9853437ab64035ba19995419a09a35d645d5dc25
jackchensm <1655664358@qq.com>
2019-07-30 15:39:24 +0800

    create table

---
+++ database/migrations/2019_07_30_073121_create_death_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeathTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('death', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char("name",50)->index()->nullable()->comment("姓名");
+            $table->string("headImg")->nullable()->comment("头像");
+            $table->string("birthAddress")->nullable()->comment("出生地");
+            $table->string("locationAddress")->nullable()->comment("常驻地");
+            $table->char("usedName",50)->nullable()->comment("小名");
+            $table->string("description")->nullable()->comment("个人简介");
+            $table->dateTime("birthday")->nullable()->comment("出生");
+            $table->dateTime("deathDay")->nullable()->comment("死亡日");
+            $table->boolean("sex")->nullable()->default(1)->comment("性别");
+            $table->unsignedInteger("userId")->index()->comment("添加人");
+            $table->dateTime("addTime")->nullable()->comment("添加时间");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('death');
+    }
+}

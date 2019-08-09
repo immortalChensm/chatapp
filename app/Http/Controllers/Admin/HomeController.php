059d6ef46e9900932b70c446bdc71869734203b3
jackchensm <1655664358@qq.com>
2019-08-09 14:37:54 +0800

    update index

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -18,6 +18,7 @@ class HomeController extends Controller
         $data['articleNum'] = DB::table("articles")->count("articleId")+DB::table("videos")->count("videoId")+DB::table("musics")->count("musicId")+DB::table("photos")->count("photoId");
         $data['shipSale'] = DB::table("users_ships_order")->where("userId","=",1)->where("type","=",2)->sum("payMoney");
         $data['spaceSale'] = DB::table("users_space_order")->sum("shipNum");
+        $data['users'] = DB::table("users")->select("name,created_at,headImgUri")->orderBy("created_at","desc")->limit(8)->get();
         return view("admin.home.index",compact('data'));
     }
 

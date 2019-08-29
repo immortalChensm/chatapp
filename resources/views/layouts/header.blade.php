98bffe8e007140f311ef8ad47297832fa7aecc54
jackchensm <1655664358@qq.com>
2019-08-29 10:17:21 +0800

    edit customer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -58,7 +58,7 @@
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="{{asset("adminlte/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
-                            <span class="hidden-xs" onclick="javascript:href='/admin/logout'">退出系统</span>
+                            <span class="hidden-xs" onclick="javascript:window.location.href='/admin/logout'">退出系统</span>
                         </a>
                         {{--<ul class="dropdown-menu">--}}
                             {{--<!-- User image -->--}}

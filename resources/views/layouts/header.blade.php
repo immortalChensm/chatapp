6110a3a3700ef97d9295f5406c49ca8d6658d066
jackchensm <1655664358@qq.com>
2019-08-29 10:04:53 +0800

    edit customer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -58,7 +58,7 @@
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="{{asset("adminlte/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs">退出系统</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->

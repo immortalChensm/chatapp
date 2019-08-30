b1b09e8f67523829d99023846ae34a1edc216cb7
jackchensm <1655664358@qq.com>
2019-08-30 11:04:24 +0800

    edit footer file

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -234,7 +234,7 @@
         var iTop = (window.screen.availHeight - 30 - iHeight) / 2;
         //获得窗口的水平位置
         var iLeft = (window.screen.availWidth - 10 - iWidth) / 2;
-        window.open(url, name, 'height=' + iHeight + ',,innerHeight=' + iHeight + ',width=' + iWidth + ',innerWidth=' + iWidth + ',top=' + iTop + ',left=' + iLeft + ',status=no,toolbar=no,menubar=no,location=no,resizable=no,scrollbars=0,titlebar=no');
+        window.open(url, name, 'height=' + iHeight + ',,innerHeight=' + iHeight + ',width=' + iWidth + ',innerWidth=' + iWidth + ',top=' + iTop + ',left=' + iLeft + ',status=no,toolbar=no,menubar=no,location=no,resizable=no,scrollbars=0,titlebar=no,location=no');
         // window.open("AddScfj.aspx", "newWindows", 'height=100,width=400,top=0,left=0,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no');
     }
 

e6c666a8c8017aa2450f4240cdf2ddde32d02326
jackchensm <1655664358@qq.com>
2019-10-07 15:39:54 +0800

    edit bus

--- resources/views/layouts/siderbar.blade.php
+++ resources/views/layouts/siderbar.blade.php
@@ -37,7 +37,7 @@
                 </ul>
             </li>
 
-            @if(array_key_exists(session("permission"),'管理员管理'))
+            @if(array_key_exists('管理员管理',session("permission")))
             <li class="treeview @if(
             preg_match('/manager/',request()->url())
             ||preg_match('/role/',request()->url())

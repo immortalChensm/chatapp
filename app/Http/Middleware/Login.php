373b00f5a0c216d9f6745e04ce926c13fce647eb
jackchensm <1655664358@qq.com>
2019-10-07 14:42:28 +0800

    edit bus

--- app/Http/Middleware/Login.php
+++ app/Http/Middleware/Login.php
@@ -42,7 +42,7 @@ class Login
                 $route = app(Route::class);
                 $action = substr($route->getActionName(),strripos($route->getActionName(),"\\")+1);
                 $permissionList = Permissions::whereIn("id",$permissionIdList)->get(['action']);
-                if (empty($permissionList)&&($action!='ManagerController@logoutHandler'||$action!='HomeController@index'))return response()->json(['code'=>0,'message'=>'你的账号没有操作权限']);
+                if (empty($permissionList)&&($action!='ManagerController@logoutHandler'||$action!='HomeController@index'))return response()->json(['code'=>0,'message'=>'你的账号没有操作权限2']);
 
                 $permissionListName = [];
                 foreach ($permissionList as $item){
@@ -52,7 +52,7 @@ class Login
                     }
                 }
                 if (!in_array($action,$permissionListName)){
-                    return response()->json(['code'=>0,'message'=>'你的账号没有操作权限']);
+                    return response()->json(['code'=>0,'message'=>'你的账号没有操作权限1']);
                 }
 
             }

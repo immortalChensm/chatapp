2e523f2dbad3175561d930664ed8fbd421bfd256
jackchensm <1655664358@qq.com>
2019-10-24 18:24:32 +0800

    recharge

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     {
         $data['userNum']    = DB::table("users")->count("userId");
         $data['articleNum'] = DB::table("articles")->count("articleId") + DB::table("videos")->count("videoId") + DB::table("musics")->count("musicId") + DB::table("photos")->count("photoId");
-        $data['shipSale']   = DB::table("users_ships_order")->where("userId", "=", 1)->where("type", "=", 2)->sum("payMoney");
+        $data['shipSale']   = DB::table("users_ships_order")->where("sellerUserId", "=", 1)->where("type", "=", 2)->sum("payMoney");
         $data['spaceSale']  = DB::table("users_space_order")->sum("shipNum");
         $data['users']      = DB::table("users")->select(["name", "created_at","headImgUrl"])->orderBy("created_at","desc")->limit(8)->get();
         $businessTotal      = DB::table("users_business")->select(["id", "state"])->count("id");

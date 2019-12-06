a8fb0b221b4fcb49171cdbbd647fa31c92431679
jackchensm <1655664358@qq.com>
2019-12-06 14:21:46 +0800

    edit admin

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -37,7 +37,7 @@ class SystemController extends Controller
             $file = substr($item->file,0,-3)."png";
             $qrCode->writeFile($file);
             $img = substr($item->uri,0,-3)."png";
-            $item->uri = "<img src='$img' width='50px' height='50px' onmouseover='function(e){consoe.log(e);}'/><div class='qrcode' style='display: none'><img src='$img' </div>";
+            $item->uri = "<img src='$img' width='50px' height='50px' onmouseover='$(this).next('div').show();'/><div class='qrcode' style='display: none'><img src='$img' </div>";
             //header('Content-Type: '.$qrCode->getContentType());
            // $item->uri = $qrCode->writeString();
 

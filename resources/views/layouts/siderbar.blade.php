f55e5eec836581d98116bb717100ceef1727ebc3
jackchensm <1655664358@qq.com>
2019-12-06 10:00:29 +0800

    edit admin

--- resources/views/layouts/siderbar.blade.php
+++ resources/views/layouts/siderbar.blade.php
@@ -261,7 +261,7 @@
                     <li @if(preg_match('/index/',request()->url())) class="active" @endif><a href="{{url("admin/system/index")}}"><i class="fa fa-circle-o"></i> 网站设置</a></li>@endif
                         @if(in_array("admin/system/top",session("permission")['系统设置']))
                     <li @if(preg_match('/top/',request()->url())) class="active" @endif><a href="{{url("admin/system/top")}}"><i class="fa fa-circle-o"></i> 置顶记录</a></li>@endif
-                    <li @if(preg_match('/top/',request()->url())) class="active" @endif><a href="{{url("admin/system/app")}}"><i class="fa fa-circle-o"></i> APP更新</a></li>@endif
+{{--                    <li @if(preg_match('/top/',request()->url())) class="active" @endif><a href="{{url("admin/system/app")}}"><i class="fa fa-circle-o"></i> APP更新</a></li>@endif--}}
                 </ul>
             </li>
             @endif

8dde3ca41b10d372ea1f9fefc5a6afaeb7a48b46
jackchensm <1655664358@qq.com>
2019-12-06 13:13:10 +0800

    edit admin

--- resources/views/admin/system/app_edit.blade.php
+++ resources/views/admin/system/app_edit.blade.php
@@ -227,6 +227,8 @@
                         file:$(this).attr("data"),
                     }
                 },"/admin/remove/upload/file");
+
+                $($($(this)[0]).parent()[0]).remove();
                 var inputUploadFile = $(".inputUploadFile");
                 $(inputUploadFile).remove();
 

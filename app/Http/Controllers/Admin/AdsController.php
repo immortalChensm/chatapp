4cf22f8cdc17e8bb3a62d90760a9a83407bd7001
jackchensm <shaodiandian88@163.com>
2019-12-19 14:38:57 +0800

    add ads module

--- app/Http/Controllers/Admin/AdsController.php
+++ app/Http/Controllers/Admin/AdsController.php
@@ -68,7 +68,7 @@ class AdsController extends Controller
 
     function remove(Ads $ads)
     {
-        $image = $ads->first();
+        $image = $ads->where("id",request()->id)->first();
         foreach ($image['uri'] as $uri){
             $file = (parse_url($uri));
             $key = (explode("/",$file['path']));

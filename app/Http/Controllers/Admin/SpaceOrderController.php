ddb440cb961c3f78f484f1a4bbafa196577bdb57
jackchensm <shaodiandian88@163.com>
2019-12-30 10:45:56 +0800

    admin

--- app/Http/Controllers/Admin/SpaceOrderController.php
+++ app/Http/Controllers/Admin/SpaceOrderController.php
@@ -27,9 +27,7 @@ class SpaceOrderController extends Controller
                 $query->where("userId","=",$searchItem['userId']);
             }
         },function (&$item){
-            if (!property_exists($item->buyer,'realName')){
-                $item->buyer = ["realName"=>""];
-            }
+
             $item->userName = (isset($item->buyer)&&property_exists($item->buyer,'realName'))?$item->buyer->realName:"";
             $item->spaceNum = $item->storageSize/1024;
             $item->sellerUserName = "平台";
